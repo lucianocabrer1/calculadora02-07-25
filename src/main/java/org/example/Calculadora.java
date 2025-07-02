e84f2bff7f0d5df28c450c47faffb4cc5f1c0f3b
lcabrera <luciano.cabrer1@gmail.com>
2025-07-02 05:48:55 -0300

    Se hace el primer push y comit

---
+++ src/main/java/org/example/Calculadora.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Calculadora {
+
+    public Calculadora(){}
+
+    public double Suma(double a, double b){
+        return a+b;
+    }
+
+    public double Resta(double a, double b){
+        return a-b;
+    }
+
+    public double multiplicacion(double a, double b){
+        return a*b;
+    }
+}
---
+++ src/test/java/TestCalculadora.java
@@ -0,0 +1,28 @@
+import org.example.Calculadora;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestCalculadora {
+
+    public Calculadora calculadora = new Calculadora();
+
+    @Test
+    public void testSuma(){
+        Assertions.assertEquals(6, calculadora.Suma(2,3));
+    }
+
+    @Test
+    public void testSumaCorrecta(){
+        Assertions.assertEquals(5, calculadora.Suma(2,3));
+    }
+
+    @Test
+    public void testMultiplicacion(){
+        Assertions.assertEquals(10,calculadora.multiplicacion(2,5));
+    }
+
+    @Test
+    public void testResta(){
+        Assertions.assertEquals(2,calculadora.Resta(5,3));
+    }
+}
